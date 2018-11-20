3521e719bcddf4b0719ffe60a7253191763c0058
robincpc <chienpinchen@gmail.com>
2018-11-19 23:36:01 -0800

    Update mqtt functions to work with MiniBot6R

--- mqtt_plugin/View/FuncPage/MQPage.Designer.cs
+++ mqtt_plugin/View/FuncPage/MQPage.Designer.cs
@@ -35,6 +35,17 @@
             this.textBox_port = new System.Windows.Forms.TextBox();
             this.btn_mqtt = new System.Windows.Forms.Button();
             this.timer_mqtt = new System.Windows.Forms.Timer(this.components);
+            this.textBox_sec = new System.Windows.Forms.TextBox();
+            this.label_sec = new System.Windows.Forms.Label();
+            this.textBox_Rec = new System.Windows.Forms.TextBox();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.label_wait = new System.Windows.Forms.Label();
+            this.textBox_wait = new System.Windows.Forms.TextBox();
+            this.btn_UdPnt = new System.Windows.Forms.Button();
+            this.textBox_LED = new System.Windows.Forms.TextBox();
+            this.label_LED = new System.Windows.Forms.Label();
+            this.timer_wait_cloud_cmd = new System.Windows.Forms.Timer(this.components);
+            this.mLB_state = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_ip
@@ -54,7 +65,7 @@
             this.textBox_ip.Name = "textBox_ip";
             this.textBox_ip.Size = new System.Drawing.Size(423, 46);
             this.textBox_ip.TabIndex = 1;
-            this.textBox_ip.Text = "broker.hivemq.com";
+            this.textBox_ip.Text = "127.0.0.1";
             // 
             // label_port
             // 
@@ -78,7 +89,7 @@
             // btn_mqtt
             // 
             this.btn_mqtt.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_mqtt.Location = new System.Drawing.Point(50, 116);
+            this.btn_mqtt.Location = new System.Drawing.Point(50, 194);
             this.btn_mqtt.Name = "btn_mqtt";
             this.btn_mqtt.Size = new System.Drawing.Size(244, 67);
             this.btn_mqtt.TabIndex = 4;
@@ -91,10 +102,141 @@
             this.timer_mqtt.Interval = 1000;
             this.timer_mqtt.Tick += new System.EventHandler(this.timer_mqtt_Tick);
             // 
+            // textBox_sec
+            // 
+            this.textBox_sec.Enabled = false;
+            this.textBox_sec.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_sec.Location = new System.Drawing.Point(330, 282);
+            this.textBox_sec.Name = "textBox_sec";
+            this.textBox_sec.Size = new System.Drawing.Size(124, 46);
+            this.textBox_sec.TabIndex = 6;
+            this.textBox_sec.Text = "5";
+            this.textBox_sec.Visible = false;
+            // 
+            // label_sec
+            // 
+            this.label_sec.AutoSize = true;
+            this.label_sec.Enabled = false;
+            this.label_sec.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_sec.Location = new System.Drawing.Point(53, 285);
+            this.label_sec.Name = "label_sec";
+            this.label_sec.Size = new System.Drawing.Size(247, 39);
+            this.label_sec.TabIndex = 5;
+            this.label_sec.Text = "Set Count (sec):";
+            this.label_sec.Visible = false;
+            // 
+            // textBox_Rec
+            // 
+            this.textBox_Rec.Enabled = false;
+            this.textBox_Rec.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Rec.Location = new System.Drawing.Point(41, 506);
+            this.textBox_Rec.Name = "textBox_Rec";
+            this.textBox_Rec.Size = new System.Drawing.Size(933, 46);
+            this.textBox_Rec.TabIndex = 7;
+            this.textBox_Rec.Text = "0";
+            this.textBox_Rec.Visible = false;
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_disconnect.Location = new System.Drawing.Point(396, 194);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(244, 67);
+            this.btn_disconnect.TabIndex = 8;
+            this.btn_disconnect.Text = "Disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // label_wait
+            // 
+            this.label_wait.AutoSize = true;
+            this.label_wait.Enabled = false;
+            this.label_wait.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_wait.Location = new System.Drawing.Point(53, 337);
+            this.label_wait.Name = "label_wait";
+            this.label_wait.Size = new System.Drawing.Size(172, 39);
+            this.label_wait.TabIndex = 9;
+            this.label_wait.Text = "Wait (sec):";
+            this.label_wait.Visible = false;
+            // 
+            // textBox_wait
+            // 
+            this.textBox_wait.Enabled = false;
+            this.textBox_wait.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_wait.Location = new System.Drawing.Point(330, 334);
+            this.textBox_wait.Name = "textBox_wait";
+            this.textBox_wait.ReadOnly = true;
+            this.textBox_wait.Size = new System.Drawing.Size(124, 46);
+            this.textBox_wait.TabIndex = 10;
+            this.textBox_wait.Text = "0";
+            this.textBox_wait.Visible = false;
+            // 
+            // btn_UdPnt
+            // 
+            this.btn_UdPnt.Enabled = false;
+            this.btn_UdPnt.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_UdPnt.Location = new System.Drawing.Point(537, 282);
+            this.btn_UdPnt.Name = "btn_UdPnt";
+            this.btn_UdPnt.Size = new System.Drawing.Size(437, 67);
+            this.btn_UdPnt.TabIndex = 11;
+            this.btn_UdPnt.Text = "Run GetUdPnt";
+            this.btn_UdPnt.UseVisualStyleBackColor = true;
+            this.btn_UdPnt.Visible = false;
+            this.btn_UdPnt.Click += new System.EventHandler(this.btn_UdPnt_Click);
+            // 
+            // textBox_LED
+            // 
+            this.textBox_LED.Enabled = false;
+            this.textBox_LED.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_LED.Location = new System.Drawing.Point(739, 380);
+            this.textBox_LED.Name = "textBox_LED";
+            this.textBox_LED.ReadOnly = true;
+            this.textBox_LED.Size = new System.Drawing.Size(172, 46);
+            this.textBox_LED.TabIndex = 13;
+            this.textBox_LED.Text = "Default";
+            this.textBox_LED.Visible = false;
+            // 
+            // label_LED
+            // 
+            this.label_LED.AutoSize = true;
+            this.label_LED.Enabled = false;
+            this.label_LED.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_LED.Location = new System.Drawing.Point(540, 383);
+            this.label_LED.Name = "label_LED";
+            this.label_LED.Size = new System.Drawing.Size(166, 39);
+            this.label_LED.TabIndex = 12;
+            this.label_LED.Text = "LED Color:";
+            this.label_LED.Visible = false;
+            // 
+            // timer_wait_cloud_cmd
+            // 
+            this.timer_wait_cloud_cmd.Interval = 1000;
+            this.timer_wait_cloud_cmd.Tick += new System.EventHandler(this.timer_wait_cloud_cmd_Tick);
+            // 
+            // mLB_state
+            // 
+            this.mLB_state.AutoSize = true;
+            this.mLB_state.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mLB_state.Location = new System.Drawing.Point(53, 129);
+            this.mLB_state.Name = "mLB_state";
+            this.mLB_state.Size = new System.Drawing.Size(102, 39);
+            this.mLB_state.TabIndex = 14;
+            this.mLB_state.Text = "State:";
+            // 
             // MQPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mLB_state);
+            this.Controls.Add(this.textBox_LED);
+            this.Controls.Add(this.label_LED);
+            this.Controls.Add(this.btn_UdPnt);
+            this.Controls.Add(this.textBox_wait);
+            this.Controls.Add(this.label_wait);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.textBox_Rec);
+            this.Controls.Add(this.textBox_sec);
+            this.Controls.Add(this.label_sec);
             this.Controls.Add(this.btn_mqtt);
             this.Controls.Add(this.textBox_port);
             this.Controls.Add(this.label_port);
@@ -115,5 +257,16 @@
         private System.Windows.Forms.TextBox textBox_port;
         private System.Windows.Forms.Button btn_mqtt;
         private System.Windows.Forms.Timer timer_mqtt;
+        private System.Windows.Forms.TextBox textBox_sec;
+        private System.Windows.Forms.Label label_sec;
+        private System.Windows.Forms.TextBox textBox_Rec;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Label label_wait;
+        private System.Windows.Forms.TextBox textBox_wait;
+        private System.Windows.Forms.Button btn_UdPnt;
+        private System.Windows.Forms.TextBox textBox_LED;
+        private System.Windows.Forms.Label label_LED;
+        private System.Windows.Forms.Timer timer_wait_cloud_cmd;
+        private System.Windows.Forms.Label mLB_state;
     }
 }
--- mqtt_plugin/View/FuncPage/MQPage.cs
+++ mqtt_plugin/View/FuncPage/MQPage.cs
@@ -23,8 +23,13 @@ namespace mqtt_plugin.View.FuncPage
         public MqttClient myMQClient = null;
         private string MQTT_BROKER_ADDRESS = "";
         private int MQTT_TCP_PORT = 0;
-        private string Parent_Topic = "NexCOBOT/MiniBot7R/";
+        private string Parent_Topic = "NexCOBOT/MiniBot6R/";
+        private string Cloud_Topic = "NexCOBOT/Cloud/";
         private string DurationString = "";
+        //  Variables for cloud command
+        private int wait_cnt = 0;
+        private int color_index = -1;
+        private string rec_payload = "";
         
 
         public MQPage(IntMQService mq_service)
@@ -33,6 +38,19 @@ namespace mqtt_plugin.View.FuncPage
             this.PageName = "MQTT";
 
             InitializeComponent();
+
+            mLB_state.Text = "Status : " + ((myMQClient == null || myMQClient.IsConnected == false) ? "Disconnect" : "Connect");
+        }
+
+        ~MQPage()
+        {
+            // Stop timer and disconnect MQTT client
+            timer_mqtt.Stop();
+            if (myMQClient != null && myMQClient.IsConnected)
+            {
+                myMQClient.Disconnect();
+                mLB_state.Text = "Status : Disconnect";
+            }
         }
 
         private void btn_mqtt_Click(object sender, EventArgs e)
@@ -48,22 +66,39 @@ namespace mqtt_plugin.View.FuncPage
                 // Connect to Broker
                 string clientId = Guid.NewGuid().ToString();
                 byte code = myMQClient.Connect(clientId);
+                mLB_state.Text = "Status : Connect";
 
                 // Start timer to publish data
+                timer_mqtt.Interval = 1000;
                 timer_mqtt.Start();
 
                 // event handler to check if message are sent to the broker successfully.
                 myMQClient.MqttMsgPublished += client_MqttMsgPublished;
 
                 // event handler to check if receive message
-                myMQClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                myMQClient.MqttMsgPublishReceived += client_MqttMsgReceived;
 
-                // Subscribe to Topic
-                //ushort msgIdrev = myMQClient.Subscribe(new string[] { Parent_Topic + "TotalPowerOnTime" }, 
-                //                                       new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }else if(myMQClient.IsConnected == false)
+            {
+                // Connect to Broker
+                string clientId = Guid.NewGuid().ToString();
+                byte code = myMQClient.Connect(clientId);
+                mLB_state.Text = "Status : Connect";
 
-                //myMQClient.Subscribe(new string[] { Parent_Topic + "JogRatio" }, 
-                //                                       new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                // Start timer to publish data
+                timer_mqtt.Interval = 1000;
+                timer_mqtt.Start();
+            }
+        }
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            // Stop timer and disconnect MQTT client
+            timer_mqtt.Stop();
+            if (myMQClient != null && myMQClient.IsConnected)
+            {
+                myMQClient.Disconnect();
+                mLB_state.Text = "Status : Disconnect";
             }
         }
 
@@ -87,7 +122,7 @@ namespace mqtt_plugin.View.FuncPage
                                               false); // retained
 
             // publish angle (deg) of each joint
-            for (int i = 0; i < 7; ++i)
+            for (int i = 0; i < 6; ++i)
             {
                 myMQClient.Publish(Parent_Topic + "Joint" + (i + 1).ToString(), // Topic
                                                   Encoding.UTF8.GetBytes(mMQService.DeviceService.GroupObjectList[0].GroupStatus.AcsActPos[i].Value.ToString()), // message body
@@ -109,7 +144,7 @@ namespace mqtt_plugin.View.FuncPage
                                               Encoding.UTF8.GetBytes(mMQService.DeviceService.GroupObjectList[0].GroupStatus.GroupState.ToString()), // message body
                                               MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
                                               false); // retained
-
+            
         }
 
         void client_MqttMsgPublished(Object sender, MqttMsgPublishedEventArgs e)
@@ -117,10 +152,59 @@ namespace mqtt_plugin.View.FuncPage
             //Console.WriteLine("MessageId = " + e.MessageId + " Published = " + e.IsPublished);
         }
 
-        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        void client_MqttMsgReceived(object sender, MqttMsgPublishEventArgs e)
         {
+            //string rec_str = "Received = " + Encoding.UTF8.GetString(e.Message) + " on topic " + e.Topic;
             //Console.WriteLine("Received = " + Encoding.UTF8.GetString(e.Message) + " on topic " + e.Topic);
+            //rec_payload = rec_str;
         }
 
+        private void btn_UdPnt_Click(object sender, EventArgs e)
+        {
+            // disable btn and reset color index
+            this.btn_UdPnt.Enabled = false;
+            color_index = -1;
+
+            // set timer interval
+            wait_cnt = System.Int32.Parse( textBox_sec.Text);
+            textBox_wait.Text = textBox_sec.Text;
+            timer_wait_cloud_cmd.Interval = 1000;  // wait 1 second to check if command send from cloud
+            timer_wait_cloud_cmd.Start();
+
+        }
+
+        private void timer_wait_cloud_cmd_Tick(object sender, EventArgs e)
+        {
+            if(wait_cnt > 0)
+            {
+                wait_cnt -= 1;
+                textBox_wait.Text = wait_cnt.ToString();
+
+                // Check if colorindex send from dashboard
+                if(color_index != -1)
+                {
+                    textBox_LED.Text = color_index.ToString();
+                    textBox_Rec.Text = rec_payload;
+                    btn_UdPnt.Enabled = true;
+                    timer_wait_cloud_cmd.Stop();
+                }
+
+            }else if(wait_cnt <= 0)
+            {
+                // Check if colorindex send from dashboard
+                if(color_index != -1)
+                {
+                    textBox_LED.Text = color_index.ToString();
+                    textBox_Rec.Text = rec_payload;
+                }else
+                {
+                    textBox_Rec.Text = "Do not receive color index! Used default color from script";
+                    textBox_LED.Text = "Default";
+                }
+                btn_UdPnt.Enabled = true;
+                timer_wait_cloud_cmd.Stop();
+            }
+
+        }
     }
 }
